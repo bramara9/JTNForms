0e63a0504b2024a8b5488967f2874a02189ee8a0
bramara9 <bramitoshop@gmail.com>
2023-08-09 23:09:53 +0530

    SalesOrder Download functionality added

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -4,6 +4,7 @@ using JTNForms.Repos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ClosedXML.Excel;
+using System.Text.RegularExpressions;
 
 namespace JTNForms.Controllers
 {
@@ -29,18 +30,42 @@ namespace JTNForms.Controllers
         public ActionResult DownloadInvoice(int customerId)
         {
             var details = GetRoomDetails(customerId);
-            var filePath = Path.GetFullPath(@"ExcelTemplate\\salesorderTemplate.xlsx");
+            var CustomerName = HttpContext.Session.GetString("username");
+            var filePath = Path.GetFullPath(@"ExcelTemplate\\SAMPLE.xlsx");
             //string filePath = "C:\\Users\\dell\\source\\repos\\GitJTF\\ExcelTemplate\\JTN Form Changes.xlsx";
             var wbook = new XLWorkbook(filePath);
-
+            var dataStartVal = 14;
             var ws = wbook.Worksheet(1);
-            ws.Cell("A14").Value = "Srini vasu";
+            var InsexVal = 1;
+            ws.Cell("AA3").Value = CustomerName;
+            ws.Cell("G3").Value = DateTime.Now.ToString("dd/MM/yyyy");
+            foreach (var windows in details)
+            {
+                ws.Cell("A" + dataStartVal).Value = InsexVal;
+                ws.Cell("B" + dataStartVal).Value = windows.RoomName;
+                ws.Cell("C" + dataStartVal).Value = windows.BlindType;
+                ws.Cell("D" + dataStartVal).Value = windows.FabricName;
+                ws.Cell("E" + dataStartVal).Value = windows.WindowName;
+                ws.Cell("F" + dataStartVal).Value = windows.Width;
+                ws.Cell("G" + dataStartVal).Value = windows.Height;
+                ws.Cell("H" + dataStartVal).Value = "Blind Size";
+                ws.Cell("I" + dataStartVal).Value = 1;
+                ws.Cell("L" + dataStartVal).Value = windows.TotalPrice;
+                ws.Cell("M" + dataStartVal).Value = (windows.IsNoValance ? "CLASSIC" : "EVO");
+                ws.Cell("N" + dataStartVal).Value = windows.ControlPosition;
+                ws.Cell("O" + dataStartVal).Value = "child safety";
+                ws.Cell("AA" + dataStartVal).Value = windows.NoOfPanels;
+                ws.Cell("AB" + dataStartVal).Value = windows.StackType;
+                ws.Cell("AD" + dataStartVal).Value = windows.ControlType + " And 2In1 Blind : " + (windows.Is2In1?"Yes":"No") ;
+                dataStartVal++;
+                InsexVal++;
+            }
             //wbook.SaveAs("C:\\Users\\dell\\source\\repos\\GitJTF\\ExcelTemplate\\JTN Form Changes2.xlsx");
             System.IO.Stream spreadsheetStream = new System.IO.MemoryStream();
             wbook.SaveAs(spreadsheetStream);
             spreadsheetStream.Position = 0;
 
-            return new FileStreamResult(spreadsheetStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") { FileDownloadName = "salesorder.xlsx" };
+            return new FileStreamResult(spreadsheetStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") { FileDownloadName = String.Concat(CustomerName.Where(c => !Char.IsWhiteSpace(c))) + ".xlsx" };
         }
 
         private void SetUserName(int customerId)
@@ -59,7 +84,7 @@ namespace JTNForms.Controllers
 
             using ( var Db = _dapperDbContext)
             {
-                lstRoomDetails = (from y in Db.Windows.Where(a => a.CustomerId == customerId)
+                lstRoomDetails = (from y in Db.Windows.Where(a => a.CustomerId == customerId && a.IsItemSelected==true)
                                   select new
                                   {
                                       BasePrice = y.BasePrice,
