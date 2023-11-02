923f533d767a2c2345ab6c87090701161e64a6a2
Bramara <bramara.r9@gmail.com>
2023-11-02 15:08:46 -0400

    Revert "few changes"
    This reverts commit 75ae79435a906cd37d89b23d28d2cd03291ee200.

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -109,7 +109,7 @@ namespace JTNForms.Controllers
                                   }).AsEnumerable().Select(y => new WindowDetails
                                   {
 
-                                      BasePrice = 1, // y.BasePrice,
+                                      BasePrice = y.BasePrice,
                                       BlindType = y.BlindType,
                                       RoomName = y.RoomName,
                                       Id = y.Id,
--- Controllers/MeasurementController.cs
+++ Controllers/MeasurementController.cs
@@ -31,7 +31,7 @@ namespace JTNForms.Controllers
             Db = dapperDbContext;
         }
 
-        public IActionResult Measurement(int customerId)
+        public IActionResult Index(int customerId)
         {
             SetUserName(customerId);
             ViewBag.userName = HttpContext.Session.GetString("username");
@@ -299,7 +299,7 @@ namespace JTNForms.Controllers
             Db.SaveChanges();
 
 
-            return RedirectToAction("Measurement", new { customerId = customerId });
+            return RedirectToAction("WindowDetails", new { customerId = customerId });
 
         }
 
